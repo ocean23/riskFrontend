6999e6bfec15925074aa7677c71dfa8eebe64dea
ocean yang <ocean.yang@ihunuo.com>
2016-09-26 17:09:35 +0800

    完成第一次集成

--- src/containers/navigation/MainPage.js
+++ src/containers/navigation/MainPage.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import classNames from 'classnames';
+import { Button } from '@sketchpixy/rubix';
 import { loginSagas } from '../../actions/userAction';
 
 class MainPage extends Component {
@@ -16,7 +17,9 @@ class MainPage extends Component {
 	render() {
 		return (
 			<div className="root-container">
-				Main Page
+			  <div><Button bsStyle="green">Green Button!</Button></div>
+			  <div><Button bsStyle="red">Red Button!</Button></div>
+			  <div><Button bsStyle="blue" outlined>Blue Button!</Button></div>
 			</div>
 			);
 	}
